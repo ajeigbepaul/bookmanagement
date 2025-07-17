92fbd7b3e528377ee330d812adaa0cf5b961569d
ajeigbepaul <pdave4krist@yahoo.com>
2025-07-17 15:27:18 +0100

    updated the book module to resolve the builderror

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { Comment } from '../comments/comment.entity';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book, Comment, User])],
   providers: [BooksService],
   controllers: [BooksController],
   exports: [BooksService],
